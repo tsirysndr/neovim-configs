961c91c8c211cad15f9a58d9ae06d0dbe7a669c1
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-08-07 04:37:54 +0000

    setup github copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,13 @@
 return {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      event = "InsertEnter",
-      config = function()
-       require("copilot").setup({})
-     end,
-  }
-
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+			},
+		})
+	end,
+}
