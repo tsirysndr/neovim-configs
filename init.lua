a1a8565f624d814fe72e842ca3e2f4f81f754826
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-12-06 07:34:23 +0000

    add lsp-zero and mason plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+
 require("config.lazy")
 
 require("toggleterm").setup{
@@ -24,3 +25,13 @@ require("toggleterm").setup{
     }
   }
 }
+
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
